b06cb46c79f86bacc0782486735f4548d708c8ec
David Cain <david.cain@pelicanengineering.com>
2025-04-12 16:56:12 -0500

    changing frequency

--- components/bno055_IMU/esp_bno055.h
+++ components/bno055_IMU/esp_bno055.h
@@ -61,7 +61,7 @@
 #define I2C_MASTER_SCL_IO (4)      //CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
 #define I2C_MASTER_SDA_IO (3)      //CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
 #define I2C_MASTER_NUM I2C_NUM_0    /*!< I2C master i2c port number */
-#define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
+#define I2C_MASTER_FREQ_HZ 50000   /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
 #define I2C_MASTER_TIMEOUT_MS 1000  /*!< I2C master timeout */
